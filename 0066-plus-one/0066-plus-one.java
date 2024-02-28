bab2ab7b2e896c8a8d046f284f2a6e345fff03af
Shraddha Gawde <shraddha.gawde1999@gmail.com>
2024-02-28 17:38:50 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (6.60%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,10 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        
         int n = digits.length;
+        
+        int[] result = new int[n + 1];
+        result[0] = 1;
         
         for(int i = n -1; i >= 0; i--){
             
@@ -12,9 +16,7 @@ class Solution {
 	        digits[i] = 0;
         }
 
-        int[] result = new int[n + 1];
         
-        result[0] = 1;
         
         return result;  
     }
